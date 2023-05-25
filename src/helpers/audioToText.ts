e65a5c1fe58dea6c7b902d75cb7cdf744e79d038
Yan <2031974+yangit@users.noreply.github.com>
2023-05-25 16:01:51 +0800

    Added retry to voice to text translation

--- src/helpers/audioToText.ts
+++ src/helpers/audioToText.ts
@@ -2,28 +2,34 @@ import axios from 'axios';
 import FormData from 'form-data';
 import fs from 'fs';
 import config from './config';
+const openAiApiKey = process.env.OPENAI_API_KEY;
+if (typeof openAiApiKey === 'undefined') {
+  throw new Error('Open AI key is undefined');
+}
+
+const getData = async (retries: number): Promise<string> => {
+  try {
+    const form = new FormData();
+    form.append('file', fs.createReadStream(config.recPath));
+    form.append('model', 'whisper-1');
+    const response = await axios.post('https://api.openai.com/v1/audio/transcriptions', form, {
+      headers: {
+        ...form.getHeaders(),
+        Authorization: `Bearer ${openAiApiKey}`,
+      },
+    });
+    return response.data.text;
+  } catch (error) {
+    // playInst()
+    if (retries === 0) {
+      throw error;
+    }
+    console.log(`Retrying voice2text Attempts remaining: ${retries - 1}`);
+    await new Promise(resolve => setTimeout(resolve, 500));
+    return await getData(retries - 1);
+  }
+};
 
 export default async (): Promise<string> => {
-  const openAiApiKey = process.env.OPENAI_API_KEY;
-  if (typeof openAiApiKey === 'undefined') {
-    throw new Error('Open AI key is undefined');
-  }
-
-  const form = new FormData();
-  form.append('file', fs.createReadStream(config.recPath));
-  form.append('model', 'whisper-1');
-
-  return await axios.post('https://api.openai.com/v1/audio/transcriptions', form, {
-    headers: {
-      ...form.getHeaders(),
-      Authorization: `Bearer ${openAiApiKey}`,
-    },
-  }).then((response) => response.data.text).then((r) => {
-    console.log('rrrrr111');
-    return r;
-  }, (err) => {
-    console.log('eeee111');
-
-    throw err;
-  });
+  return await getData(3);
 };
--- src/helpers/cursorToVar.ts
+++ src/helpers/cursorToVar.ts
@@ -2,10 +2,8 @@ import clipToVar from './clipToVar';
 import cursorToClip from './cursorToClip';
 import varToClip from './varToClip';
 
-export default async (): Promise<string> => await varToClip('').then(async () => {
-//   console.log('varToClip done');
+export default async (): Promise<string> => {
+  await varToClip('');
   await cursorToClip();
-}).then(async () => {
-//   console.log('cursorToClip done');
   return await clipToVar();
-});
+};

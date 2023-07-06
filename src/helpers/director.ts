c4a1356d7e5d823545756b8fe95eea09f9d48e83
Yan <2031974+yangit@users.noreply.github.com>
2023-07-06 11:51:50 +0800

    remove voice

--- src/helpers/director.ts
+++ src/helpers/director.ts
@@ -25,25 +25,104 @@ const tick = async (): Promise<void> => {
   if (!state.get().running && clicks.length > currentIndex + 1) {
     state.set('running', true);
     currentIndex += 1;
-
     if (state.get().alfredClick === 'rest') {
       playInst('click.wav');
-      try {
-        console.log('unlinking remove', config.recPath);
-        fs.unlinkSync(config.recPath);
-      } catch { }
-      await Promise.all([
-        cursorToVar().then((text) => {
-          console.log('setting clip beofre rec', text);
-          state.set('clip', text);
-        }),
-        recordStart(),
-      ]);
-      state.set('alfredClick', 'recording');
-      state.set('running', false);
-      nextClick();
-      return;
+      await cursorToVar().then((text) => {
+        console.log('setting clip beofre rec', text);
+        state.set('clip', text);
+      });
+
+      const clip = state.get().clip;
+      const voice = '';
+
+      console.log('Siri mode', state.get());
+
+      if (clip.startsWith('-l')) {
+        console.log('Listing conversations');
+
+        const conversaionList = permaState.get().map(
+          (conversation, cIndex) => `-c:${cIndex} ${getUserQuestion(conversation.messages).slice(0, 200)}`,
+        ).join('\n\n');
+        await varToCursor(conversaionList).then(() => { }, (err) => { throw err; });
+        state.set('alfredClick', 'rest');
+        state.set('running', false);
+        nextClick();
+        return;
+      }
+      const minusC = /^-c:(\d+)(?: |\n|$)/;
+      if (minusC.test(clip)) {
+        console.log('Opening conversation');
+        const conversationId = clip.match(minusC)?.[1];
+        if (typeof conversationId === 'string') {
+          const conversationFile: string = path.resolve(config.answerPath, `c${conversationId}.txt`);
+          await exec(`open -a ${config.tailApp} ${conversationFile}`);
+        }
+
+        state.set('alfredClick', 'rest');
+        state.set('running', false);
+        nextClick();
+        return;
+      }
+      // conversation
+      let conversation: Conversation;
+
+      if (isCursor(clip.split('\n')[0])) {
+        // detect if we are continuing a conversation
+        conversation = getConversation(clip.split('\n')[0]);
+
+        if (voice.length > 0) {
+          conversation.messages.push({ role: 'user', content: voice });
+        }
+        if (clip.split('\n').length > 1) {
+          conversation.messages.push({ role: 'user', content: clip.split('\n').slice(1).join('\n') });
+        }
+        if (clip.split('\n').length > 1 || voice.length > 0) {
+          askToFile(conversation, () => { playInst('chimes.mp3'); }).then(() => { playInst('done.mp3'); }, handler);
+        } else {
+          console.log('No voice or clip');
+          playInst('water.mp3');
+        }
+        state.set('alfredClick', 'rest');
+        state.set('running', false);
+        nextClick();
+        return;
+      } else {
+        // start a new conversation
+        conversation = { model, messages: [] };
+        if (clip.length > 0) {
+          conversation.messages.push({ role: 'user', content: clip });
+        }
+        if (clip.length > 0 || voice.length > 0) {
+          permaState.add(conversation);
+          // throw new Error('Sho return!!'); // eslint-disable-line
+          askToFile(conversation, () => { playInst('chimes.mp3'); }).then(() => { playInst('chimes.mp3'); }, handler);
+        } else {
+          console.log('No voice or clip');
+        }
+        state.set('alfredClick', 'rest');
+        state.set('running', false);
+        nextClick();
+        return;
+      }
     }
+    // if (state.get().alfredClick === 'rest') {
+    //   playInst('click.wav');
+    //   try {
+    //     console.log('unlinking remove', config.recPath);
+    //     fs.unlinkSync(config.recPath);
+    //   } catch { }
+    //   await Promise.all([
+    //     cursorToVar().then((text) => {
+    //       console.log('setting clip beofre rec', text);
+    //       state.set('clip', text);
+    //     }),
+    //     recordStart(),
+    //   ]);
+    //   state.set('alfredClick', 'recording');
+    //   state.set('running', false);
+    //   nextClick();
+    //   return;
+    // }
 
     if (state.get().alfredClick === 'recording') {
       playInst('sending.mp3');
